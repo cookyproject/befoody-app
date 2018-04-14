ff8ba19485f3d4a999876cd4e4f8fa660f32cede
Debbiechen <debbie.cc.chen@gmail.com>
2018-04-14 17:12:07 +0800

    fix bugs

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic', 'firebase', 'monospaced.elastic', 'ngGuid', 'ngCordova','ion-google-place'])
+var app = angular.module('starter', ['ionic', 'firebase', 'monospaced.elastic', 'ngGuid', 'ngCordova'])
   .run(function ($ionicPlatform, $rootScope, $firebaseAuth, $state) {
     
     $ionicPlatform.ready(function () {
