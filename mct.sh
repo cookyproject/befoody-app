875788ec24b5fdaea5fe0c2eade935126def13eb
Debbiechen <debbie.cc.chen@gmail.com>
2018-05-02 16:38:22 +0800

    add background image dire

---
+++ mct.sh
@@ -0,0 +1,11 @@
+rm -rf "$(git rev-parse --git-dir)/refs/original/"
+
+commit="$1"
+date=$(date -d "$2" +%s)
+git filter-branch --env-filter \
+    "if [ \$GIT_COMMIT = '$commit' ]; then
+         export GIT_AUTHOR_DATE='$date'
+         export GIT_COMMITTER_DATE='$date'
+         export GIT_COMMITTER_NAME="\$GIT_AUTHOR_NAME"
+         export GIT_COMMITTER_EMAIL="\$GIT_AUTHOR_EMAIL"
+     fi"
---
+++ www/js/directive/back-img.js
@@ -0,0 +1,9 @@
+app.directive('backImg', function () {
+  return function (scope, element, attrs) {
+    attrs.$observe('backImg', function (value) {
+      element.css({
+        'background-image': 'url(' + value + ')'
+      });
+    });
+  };
+})
