2ae824954af64b2403b2bf4818d3e0d058a5a064
Debbiechen <Debbie.cc.chen@gmail.com>
2018-11-25 10:59:07 +0800

    add geolocation plugin

--- www/js/place-map.js
+++ www/js/place-map.js
@@ -1,10 +1,18 @@
 app.controller('PlaceMapCtrl', function ($scope, $rootScope, $state, $http, $ionicPopup, $ionicHistory) {
 
+  $scope.map = null;
   $scope.goBackward = function () {
     $ionicHistory.goBack();
   };
   $scope.init = function () {
-    
+
+    $scope.map = new google.maps.Map($('div.map')[0], {
+      center: {
+        lat: -34.397,
+        lng: 150.644
+      },
+      zoom: 15
+    });
   };
   $scope.init();
 });
