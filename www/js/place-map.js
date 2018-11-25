33fdfa9e54aacbac0d4024479cc5a5a53e23a325
Debbiechen <Debbie.cc.chen@gmail.com>
2018-11-25 12:06:45 +0800

    scroll card

--- www/js/place-map.js
+++ www/js/place-map.js
@@ -1,25 +1,43 @@
 app.controller('PlaceMapCtrl', function ($scope, $rootScope, $state, $http, $ionicPopup, $ionicHistory) {
 
   $scope.map = null;
+  $scope.currLoc = {
+    lat: 25.0444082,
+    lng: 121.522801
+  };
   $scope.goBackward = function () {
     $ionicHistory.goBack();
   };
   $scope.$on("$ionicView.enter", function (scopes, states) {
-    var cuurLoc = {
-      lat: 25.0444082,
-      lng: 121.522801
-    };
+
     if (!$scope.map) {
       $scope.map = new google.maps.Map($('div.map')[0], {
-        center: currLoc,
-        zoom: 15
+        center: $scope.currLoc,
+        zoom: 15,
+        mapTypeControl: false,
+        zoomControl: false,
+        streetViewControl: false,
+        fullscreenControl: false
       });
     }
 
     navigator.geolocation.getCurrentPosition(function (position) {
       console.log(position);
+      if (position) {
+        $scope.currLoc = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+      }
+      $scope.map.panTo($scope.currLoc);
+
+
     }, function (err) {
       console.error(err);
+    }, {
+      timeout: 30000,
+      maximumAge: 0,
+      enableHighAccuracy: true
     });
 
   });
